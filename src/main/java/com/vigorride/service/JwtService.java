515e4be75b1208071abb7b55bc42e2f58b0cb1c2
rr06101998 <ramraj06101998@gmail.com>
2023-08-26 01:12:32 +0530

    application setup

--- src/main/java/com/vigorride/service/JwtService.java
+++ src/main/java/com/vigorride/service/JwtService.java
@@ -3,5 +3,13 @@ package com.vigorride.service;
 public interface JwtService {
 	
     String createJwtToken(String username);
+
+	Boolean isJwtTokenValid(String jwtToken);
+
+	String getUserName(String jwtToken);
+
+	String getUserNameInBearer(String rawToken);
+
+	String getCurrentUser();
 	
 }
